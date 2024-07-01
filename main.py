0a3f8491950dbb014571bde4ad25cbbb9d31a12a
Reuben <reuben.barretto8@gmai.com>
2024-07-01 15:33:10 +0530

    Add description on the new columns

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ with col2:
     st.title("Reuben Barretto")
     content = """
     I am Reuben Barretto a python programmer.i have graduated in  2025 with a 
-    computer science degree. I am currently a student working on various projects
+    computer science degree. I am currently a student working on various projects.
+    With a strong foundation in Python's extensive ecosystem, I've developed 
+    expertise in various fields such as web development, data analysis,automation,
+    and machine learning. I'm committed to continuous learning and staying up-to-date with the
+    latest trends and technologies, ensuring that I'm always at the cutting edge of innovation. 
+    Whether it's building robust applications or crafting efficient scripts,
+    my meticulous attention to detail and creative approach help me
+    deliver exceptional results and contribute effectively to any team.
     """
     st.info(content)
+
+content2 = """
+    Below you can find some of the apps i have built in python.Please feel free 
+    to contact me
+    """
+st.write(content2)

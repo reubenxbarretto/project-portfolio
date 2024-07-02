daa808ee11cb09ee6dcd1ac183fa4438eaea223c
Reuben <reuben.barretto8@gmai.com>
2024-07-02 12:33:20 +0530

    Send Email using forums

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,11 +1,18 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Us")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your Email Address")
-    message = st.text_area("Your Message")
+    raw_message = st.text_area("Your Message")
+    message = f"""\
+    Subject:New Email From {user_email}
+    From: {user_email}
+    {raw_message}
+    """
     button = st.form_submit_button("Submit")
     print(button)
     if button:
-        print("Pressed")
+        send_email(message)
+        st.info("Your Email Was Sent Successfully")
---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "reuben.barretto8@gmail.com"
+    password = "twhomljwavzrhgrl"
+
+    receiver = "reuben.barretto8@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+
